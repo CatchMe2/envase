2743f4b5bda7fd3983e4ea51a3defe16ab9af042
Mateusz Tkacz <mateusz.tkacz94.dev@gmail.com>
2025-04-08 11:06:29 +0200

    chore: simplify type infer

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,18 @@
 import type { StandardSchemaV1 } from '@standard-schema/spec';
 
-export type EnvvarEntry<T extends StandardSchemaV1 = StandardSchemaV1> = [
-  string,
-  T,
-];
+export type EnvvarEntry<T extends StandardSchemaV1> = [string, T];
 
 export type EnvSchema = {
-  [key: string]: EnvSchema | EnvvarEntry;
+  [key: string]: EnvSchema | EnvvarEntry<StandardSchemaV1>;
 };
 
-type DepthLevels = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export type InferEnv<
-  T extends EnvSchema,
-  DepthLimit extends number = 10,
-> = DepthLimit extends 0
-  ? never
-  : {
-      [K in keyof T]: T[K] extends EnvvarEntry<infer Schema>
-        ? StandardSchemaV1.InferOutput<Schema>
-        : T[K] extends EnvSchema
-          ? InferEnv<T[K], DepthLevels[DepthLimit]>
-          : never;
-    };
+export type InferEnv<T extends EnvSchema> = {
+  [K in keyof T]: T[K] extends EnvvarEntry<infer Schema>
+    ? StandardSchemaV1.InferOutput<Schema>
+    : T[K] extends EnvSchema
+      ? InferEnv<T[K]>
+      : never;
+};
 
 export type NodeEnvInfo = {
   isProduction: boolean;
