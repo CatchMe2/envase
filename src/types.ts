96055a886f0d6c7af098ca3da6f2a03e5f5acf7f
Mateusz Tkacz <84970625+CatchMe2@users.noreply.github.com>
2025-05-02 17:08:57 +0200

    feat: move node env flag detection to dedicated function (#5)
    * feat: move node env flag detection to a dedicated function

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
 import type { SimplifyDeep } from 'type-fest';
 import type { StandardSchemaV1 } from './standard-schema.ts';
 
+export type NodeEnvInfo = {
+  isProduction: boolean;
+  isTest: boolean;
+  isDevelopment: boolean;
+};
+
 export type EnvvarEntry<T extends StandardSchemaV1> = [string, T];
 
 export type EnvSchema = {
@@ -16,16 +22,6 @@ type RecursiveInferEnv<T extends EnvSchema> = {
 };
 
 export type InferEnv<T extends EnvSchema> = SimplifyDeep<RecursiveInferEnv<T>>;
-
-export type NodeEnvInfo = {
-  isProduction: boolean;
-  isTest: boolean;
-  isDevelopment: boolean;
-};
-
-export type ParseEnvOutput<T extends EnvSchema> = SimplifyDeep<
-  RecursiveInferEnv<T> & NodeEnvInfo
->;
 
 export type EnvvarValidationIssue = {
   name: string;
